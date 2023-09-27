b161c52a2e98c2dbd4c42431eb751bb4e8b9d3c3
srinu <srinu1544@gmail.com>
2023-09-27 16:26:37 +0530

    project added practice app

--- app/src/main/java/com/example/practiceapp/functions/infix.kt
+++ app/src/main/java/com/example/practiceapp/functions/infix.kt
@@ -3,9 +3,7 @@ package com.example.practiceapp.functions
  /* In kotlin an infix function is a function that allows to to call it with special syntax, with out using
  the traditional dot-notation and parenthesis. To declare an infix function, you use the infix modifier when
   defining the function. Infix functions are typically used to make code more readable and expressive,
-  especially when working with operations that mimic natural language.
-
- */
+  especially when working with operations that mimic natural language. */
 
 
 infix fun Int.add(other : Int) : Int {
